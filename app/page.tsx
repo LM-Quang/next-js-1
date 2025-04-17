7f78acfba525b57115ab97b69a2c25b6e7cc7256
LM-Quang <minh.quangle15111995@gmail.com>
2025-04-17 22:13:43 +0900

    set up for the food app

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,7 @@
-import FavoritesContext from "@/store/favorites-context";
-import { useContext } from "react";
-
-interface HomeProps {
-   id: string;
-}
-
-export default function Home(props: HomeProps) {
-   const favoritesCtx = useContext(FavoritesContext);
-   const isItemFavorite = favoritesCtx.isItemFavorite(props.id);
-
-   function toggleFavoriteStatusHandle() {
-      if (isItemFavorite) {
-         favoritesCtx.removeFavorite(props.id);
-      } else {
-         favoritesCtx.addFavorite({ id: "123", name: "Quang" });
-      }
-   }
-
+export default function Home() {
    return (
-      <div>
-         <button onClick={toggleFavoriteStatusHandle}>
-            {isItemFavorite ? "Remove from Favorites" : "Add to Favorites"}
-         </button>
-      </div>
+      <main>
+         <h1 className="text-white text-center">Time to get started!</h1>
+      </main>
    );
 }

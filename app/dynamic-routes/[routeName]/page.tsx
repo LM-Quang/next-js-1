2dc87c9fc55af5b5738b819ac9c706db82c6c155
LM-Quang <minh.quangle15111995@gmail.com>
2025-04-19 22:54:37 +0900

    update dynamic-route, using useParams hook

--- app/dynamic-routes/[routeName]/page.tsx
+++ app/dynamic-routes/[routeName]/page.tsx
@@ -1,15 +1,13 @@
-import React from "react";
+"use client";
 
-type PageProps = {
-   params: {
-      routeName: string;
-   };
-};
+import { useParams } from "next/navigation";
+import React from "react";
 
 // The "routeName" is the name of the [routeName] folder that will show up in the URL
 // Ex: http://localhost:3000/dynamic-routes/route-1
 // => routeName = route-1
-const RoutePage = ({ params }: PageProps) => {
+const RoutePage = () => {
+   const params = useParams<{ routeName: string }>();
    return (
       <div>
          <p>{params.routeName}</p>
